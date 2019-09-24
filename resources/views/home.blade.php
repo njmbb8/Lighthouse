5e2340d10417641311e77018421646e918e4fb9f
vagrant <vagrant@homestead>
2019-09-24 21:47:32 +0000

    homepage is working

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+	<div class="jumbotron" id="banner">
+		<div class="container text-center my-auto">
+				<h1>Individualized learning options</h1>
+				<h2>because all students are individuals.</h2>
+		</div>
+	</div>
+@endsection
---
+++ resources/views/layouts/partials/navbar.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+	<div class="container">
+		<a class="navbar-brand" href="#">
+			<img src="/images/logo.png" style="width:60px;">Lighthouse of Learning
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+		      <span class="navbar-toggler-icon"></span>
+		</button>
+	    <div class="collapse navbar-collapse" id="navbarResponsive">
+	      <ul class="navbar-nav ml-auto">
+	        <li class="nav-item active">
+	          <a class="nav-link" href="#">Home
+	                <span class="sr-only">(current)</span>
+	              </a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#about">About Us</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#staff">Staff</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#contact">Contact Us</a>
+	        </li>
+	        <li class="nav-item">
+	        	<a class="nav-link" href="#activities">Activities</a>
+	        </li>
+	        <li class="nav-item">
+	        	<a class="nav-link" href="forms.html">Forms</a>
+	        </li>
+	      </ul>
+	    </div>
+	</div>
+</nav>
