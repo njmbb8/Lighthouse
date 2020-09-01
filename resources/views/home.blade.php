4ebbd68a902e97d7fc6a0dcae9dd97bd8066afbe
Nate McGraw <nmcgr001@gmail.com>
2020-09-01 19:18:09 -0400

    changed hero image to carousel and added images from lighthouse

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,51 @@
 @section('title', 'Home')
 
 @section('content')
-	<div class="jumbotron" id="banner">
-		<div class="container text-center my-auto">
-				<h1>Individualized learning options</h1>
-				<h2>because all students are individuals.</h2>
+	<div id="carouselExampleIndicators" class="carousel slide bg-secondary" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block mx-auto" src="/images/0.jpeg" alt="First slide">
+				<div class="carousel-caption d-none d-md-block">
+					<h1>Individualized learning options</h1>
+					<h2>because all students are individuals.</h2>
+  				</div>
+			</div>
+			<div class="carousel-item">
+				<img class="d-block mx-auto" src="/images/0-1.jpeg" alt="Second slide">
+				<div class="carousel-caption d-none d-md-block">
+					<h1>Individualized learning options</h1>
+					<h2>because all students are individuals.</h2>
+  				</div>
+			</div>
+			<div class="carousel-item">
+				<img class="d-block mx-auto" src="/images/0-2.jpeg" alt="Third slide">
+				<div class="carousel-caption d-none d-md-block">
+					<h1>Individualized learning options</h1>
+					<h2>because all students are individuals.</h2>
+  				</div>
+			</div>
+			<div class="carousel-item">
+				<img class="d-block mx-auto" src="/images/0-3.jpeg" alt="Third slide">
+				<div class="carousel-caption d-none d-md-block">
+					<h1>Individualized learning options</h1>
+					<h2>because all students are individuals.</h2>
+  				</div>
+			</div>
 		</div>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
 	</div>
 	<div class="container">
 		<div class="row">
